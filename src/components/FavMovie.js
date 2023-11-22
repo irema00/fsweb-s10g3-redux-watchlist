6eb49f30b8afd23e61dd9fc8f0448040af0bd205
İrem Akın Tunga <iremakin00@gmail.com>
2023-11-23 02:51:42 +0300

    adds removeFromList handler

--- src/components/FavMovie.js
+++ src/components/FavMovie.js
@@ -1,8 +1,16 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { removeFromList } from "./../actions/favoritesActions";
 export default function FavMovie({ title, id }) {
+  const dispatch = useDispatch();
+
+  const removeFromListHandler = () => {
+    dispatch(removeFromList(id));
+  };
   return (
     <div className="flex p-3 pl-4 bg-white mb-2 shadow items-center group">
       <div className="pr-4 flex-1">{title}</div>
-      <button className="px-3 py-2 block text-sm rounded bg-rose-700 text-white opacity-30 group-hover:opacity-100">
+        onClick={removeFromListHandler}
         Çıkar
       </button>
     </div>
